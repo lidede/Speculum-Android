eb15d5436d4f54a0ef3ad97412513d93dc1b1173
collins lidede <lidedec@hotmail.com>
2016-12-29 16:53:08 -0500

    Add files via upload

--- app/src/main/java/com/nielsmasdorp/speculum/views/MainView.java
+++ app/src/main/java/com/nielsmasdorp/speculum/views/MainView.java
@@ -2,6 +2,7 @@ package com.nielsmasdorp.speculum.views;
 
 import com.nielsmasdorp.speculum.models.RedditPost;
 import com.nielsmasdorp.speculum.models.Weather;
+import com.nielsmasdorp.speculum.models.QuotePost;
 
 /**
  * @author Niels Masdorp (NielsMasdorp)
@@ -19,6 +20,8 @@ public interface MainView extends BaseView {
 	void displayCurrentWeather(Weather weather, boolean isSimpleLayout);
 
 	void displayCalendarEvents(String events);
+
+	void displayQuotePost(QuotePost quotePost);
 
 	void displayTopRedditPost(RedditPost redditPost);
 }
